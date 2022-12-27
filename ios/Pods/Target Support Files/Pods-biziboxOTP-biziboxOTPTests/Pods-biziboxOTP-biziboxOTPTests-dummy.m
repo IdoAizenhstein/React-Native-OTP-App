d7c63f064b3f92931f1213a069280e05c86b5207
IdoAizenhstein <idoaizenshtein@gmail.com>
2022-12-26 22:35:08 -0500

    INIT

--- ios/Pods/Target Support Files/Pods-biziboxOTP-biziboxOTPTests/Pods-biziboxOTP-biziboxOTPTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_biziboxOTP_biziboxOTPTests : NSObject
-@end
-@implementation PodsDummy_Pods_biziboxOTP_biziboxOTPTests
-@end
--- ios/Pods/Target Support Files/Pods-biziboxOTP/Pods-biziboxOTP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_biziboxOTP : NSObject
-@end
-@implementation PodsDummy_Pods_biziboxOTP
-@end
